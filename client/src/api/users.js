42c76c6f65378b54b27fca85e12f81ef5cbaf252
Renier Santander <rsantander@incomm.com>
2020-07-21 15:29:12 -0400

    forgot to include user api client

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -1,23 +1,15 @@
 import http from './config'
 
 export async function signUp(payload) {
-    try {
-        const response = await http.post('signup', {
-            username: payload.username,
-            password: payload.password,
-            email: payload.email,
-            firstname: payload.firstname,
-            lastname: payload.lastname
-        });
-        return true;
-    } catch (error) {
-        return false;
-    }
+    return await http.post('signup', {
+        username: payload.username,
+        password: payload.password
+    });
 }
 
 export async function login(payload) {
     try {
-        const response = await http.post('login', {
+        await http.post('login', {
             username: payload.username,
             password: payload.password
         });
